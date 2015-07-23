d6e95c95176bffb9aebada239e9fc567c4528a7b
Patrick Burke <hi@pburke.de>
2015-07-24 01:34:25 +0200

    Project and requirement model + views and migrations.
    Some project scaffolding, nothing happened really.

---
+++ database/migrations/2015_07_23_224801_create_requirements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('per_project_id')->unique();
+            $table->text('text');
+            $table->integer('project_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requirements');
+    }
+}
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+	<div class="container">
+		Ich bin ein kleiner Test
+	</div>
+@endsection
